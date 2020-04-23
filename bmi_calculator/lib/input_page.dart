62c77a1d5e4fde400af8c987420c30484e857a79
Naman Bhoj <39219774+NamanBhoj@users.noreply.github.com>
2020-04-24 04:59:06 +0530

    Update input_page.dart

--- bmi_calculator/lib/input_page.dart
+++ bmi_calculator/lib/input_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
-
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 const bottomcontainerheight = 80.0;
